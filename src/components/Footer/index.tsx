f91cfcc59e3b43cc7eebf2b8f807a0d8ce95aa72
Hiago Prates <hiagohps360@gmail.com>
2022-08-02 13:08:09 -0300

    feat: component footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import { Container } from './styles';
+
+const Footer: FC = () => (
+  <Container>
+    <span>© 2022 MovieFlix</span>
+  </Container>
+);
+
+export default Footer;
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 120px;
+  background-color: #111411;
+
+  color: #fff;
+
+  margin-top: auto;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
